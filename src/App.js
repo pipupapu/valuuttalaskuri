f74e67772ccdd284a314e7c2f45695950ab9cbc9
pipupapu <127877505+pipupapu@users.noreply.github.com>
2023-10-13 20:06:23 +0300

    committia

--- src/App.js
+++ src/App.js
@@ -2,34 +2,33 @@ import { useState } from 'react';
 import './App.css';
 import axios from 'axios';
 
-const URL =  'https://api.freecurrencyapi.com/v1/latest'
+const URL = 'https://api.freecurrencyapi.com/v1/latest'
 //?apikey=fca_live_1lHSJSycVdNzvsmmCk5HLxBQePLAFnFJDqFbt1Gw&currencies=GBP&base_currency=EUR'
 
 
-function App() {//export default function currency
-  const [eur, setEur] = useState(0)//vaihda tähän Eur
-  const [gbp, setGbp] = useState(0)//ja tähän Gpb ja muuta samat myös labeleihin
+function App() {
+  const [eur, setEur] = useState(0)
+  const [gbp, setGbp] = useState(0)
   const [rate, setRate] = useState(0)
 
-  
-  const convert = (e) => {//kokeile tähän handleSubmit, uusi alkaa const convert,tehty mallin mukaan, hae axios ja url
+
+  const convert = (e) => {
     e.preventDefault()
     axios.get(URL)
-     .then((response) => {
+      .then((response) => {
         const json = response.data
         setRate(json.data.GBP)
         setGbp(eur * json.data.GBP)
       }).catch(error => {
         alert(error)
       })
-
   }
-  return (//tee pari id:tä lisää, joilla määrittelet muotoiluja css:ssä ja calc->convertyms.katso tehtävä 
+  return (
     <div id="container">
       <form onSubmit={convert}>
         <div>
           <label>EUR</label>
-          <input type="number" step="0.01" value={eur} onChange={e => setEur(e.target.value)} /> 
+          <input type="number" step="0.01" value={eur} onChange={e => setEur(e.target.value)} />
           <output>{rate}</output>
         </div>
         <div>
@@ -37,7 +36,7 @@ function App() {//export default function currency
           <output>{gbp.toFixed(2)} £</output>
         </div>
         <div>
-         <button>Calculate</button>
+          <button>Calculate</button>
         </div>
       </form>
     </div>
